8a9080990e656b97230c54290175249755faa08b
Sergey Kalinovsky <lord.skiminok@gmail.com>
2022-02-16 15:45:27 +0100

    Added dynamic color support

---
+++ app/src/main/java/com/criticalgnome/materialyoudemo/App.kt
@@ -0,0 +1,11 @@
+package com.criticalgnome.materialyoudemo
+
+import android.app.Application
+import com.google.android.material.color.DynamicColors
+
+class App: Application() {
+    override fun onCreate() {
+        DynamicColors.applyToActivitiesIfAvailable(this)
+        super.onCreate()
+    }
+}
