7dd486251f764a3d0a50bcdbff100a482ec99105
Sergey Kalinovsky <lord.skiminok@gmail.com>
2022-02-16 16:05:44 +0100

    Added override coloring

--- app/src/main/java/com/criticalgnome/materialyoudemo/App.kt
+++ app/src/main/java/com/criticalgnome/materialyoudemo/App.kt
@@ -6,6 +6,8 @@ import com.google.android.material.color.DynamicColors
 class App: Application() {
     override fun onCreate() {
         DynamicColors.applyToActivitiesIfAvailable(this)
+        // Colors overriding, for example if we need branding colors for some elements
+        // DynamicColors.applyToActivitiesIfAvailable(this, R.style.MaterialYouDemo_Overlay)
         super.onCreate()
     }
 }
