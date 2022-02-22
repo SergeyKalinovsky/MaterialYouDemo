4761c0221f108025bde626a80bd2df02dce441e7
Sergey Kalinovsky <lord.skiminok@gmail.com>
2022-02-22 10:14:08 +0100

    Using `with()` to make code more readable

--- app/src/main/java/com/criticalgnome/materialyoudemo/MainActivity.kt
+++ app/src/main/java/com/criticalgnome/materialyoudemo/MainActivity.kt
@@ -23,40 +23,44 @@ class MainActivity : AppCompatActivity() {
             cardText.setText(R.string.lorem_ipsum)
 
             val input1MaxLength = 10
-            textInput1.setHint(R.string.enter_text)
-            textInput1.counterMaxLength = input1MaxLength
-            textInput1.isCounterEnabled = true
-            textInput1.editText?.doAfterTextChanged { editable ->
-                textInput1.error = editable.validate(input1MaxLength)
+            with(textInput1) {
+                setHint(R.string.enter_text)
+                counterMaxLength = input1MaxLength
+                isCounterEnabled = true
+                editText?.doAfterTextChanged { error = it.validate(input1MaxLength) }
             }
 
             val input2MaxLength = 20
-            textInput2.setHint(R.string.enter_another_text)
-            textInput2.counterMaxLength = input2MaxLength
-            textInput2.isCounterEnabled = true
-            textInput2.editText?.doAfterTextChanged { editable ->
-                textInput2.error = editable.validate(input2MaxLength)
+            with(textInput2) {
+                setHint(R.string.enter_another_text)
+                counterMaxLength = input2MaxLength
+                isCounterEnabled = true
+                editText?.doAfterTextChanged { error = it.validate(input2MaxLength) }
             }
 
             switch1Label.setText(R.string.first_option)
             switch2Label.setText(R.string.second_option)
 
-            button1.text = getString(R.string.save_changes)
-            button1.setOnClickListener {
-                AlertDialog.Builder(this@MainActivity)
-                    .setTitle(R.string.dialog_title)
-                    .setMessage(R.string.lorem_ipsum_short)
-                    .setPositiveButton(android.R.string.ok, null)
-                    .setNegativeButton(android.R.string.cancel, null)
-                    .show()
+            with(button1) {
+                text = getString(R.string.save_changes)
+                setOnClickListener {
+                    AlertDialog.Builder(this@MainActivity)
+                        .setTitle(R.string.dialog_title)
+                        .setMessage(R.string.lorem_ipsum_short)
+                        .setPositiveButton(android.R.string.ok, null)
+                        .setNegativeButton(android.R.string.cancel, null)
+                        .show()
+                }
             }
 
-            button2.text = getString(android.R.string.cancel)
-            button2.setOnClickListener {
-                Snackbar
-                    .make(binding.root, getString(R.string.sample_text), Snackbar.LENGTH_INDEFINITE)
-                    .setAction(android.R.string.ok, null)
-                    .show()
+            with(button2) {
+                text = getString(android.R.string.cancel)
+                button2.setOnClickListener {
+                    Snackbar
+                        .make(binding.root, getString(R.string.sample_text), Snackbar.LENGTH_INDEFINITE)
+                        .setAction(android.R.string.ok, null)
+                        .show()
+                }
             }
         }
     }
